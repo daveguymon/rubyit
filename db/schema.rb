7299c8d94d5340454532db15deb4bd9e7d33314c
Dave Guymon <davidwguymon@gmail.com>
2020-03-07 20:22:17 -0700

    Changed db appname reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_161515) do
+ActiveRecord::Schema.define(version: 2020_03_08_031344) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -18,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_03_07_161515) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
