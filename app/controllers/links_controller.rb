e34646d3352878b5539a630638397d16b4e90527
Dave Guymon <davidwguymon@gmail.com>
2020-03-08 22:54:02 -0600

    Check user authentication for edit and destroy

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_link, only: [:show, :edit, :update, :destroy]
 
   # GET /links
