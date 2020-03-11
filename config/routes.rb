c567b06939ac85df68e4fa0f0aa1522e4dce7967
Dave Guymon <davidwguymon@gmail.com>
2020-03-10 22:31:15 -0600

    Moves root to to top of block

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "links#index"
+  
   resources :comments
   devise_for :users
   resources :links do
@@ -8,7 +10,4 @@ Rails.application.routes.draw do
     end
     resources :comments
   end
-
-  root to: "links#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
